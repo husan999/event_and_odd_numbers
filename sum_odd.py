b008abbfc75c43edf1b4df539f95e37eb658ba16
husan999 <husanshunqorov07@gmail.com>
2023-02-20 20:52:15 -0700

    good

--- sum_odd.py
+++ sum_odd.py
@@ -5,11 +5,11 @@
 
 #Find the sum of the odd digits in the variable "var_int".
 var_int=7293
-sum_even=0
+sum_odd=0
 a1=var_int%10
 a2=var_int%100//10
 a3=var_int//100%10
 a4=var_int//1000
-sum_even=(a1)%2*a1+(a2)%2*a2+(a3)%2*a3+(a4)%2*a4
-print(sum_even)
+sum_odd=(a1)%2*a1+(a2)%2*a2+(a3)%2*a3+(a4)%2*a4
+print(sum_odd)
 
